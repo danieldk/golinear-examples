d93613e6241488e084481afca81d3e80984b0c68
Daniël de Kok <me@danieldk.eu>
2012-09-14 19:19:19 +0200

    Move examples to a separate repository.

---
+++ examples/word_classification/util.go
@@ -0,0 +1,18 @@
+package word_classification
+
+import (
+	"bufio"
+)
+
+func ReadLn(r *bufio.Reader) (string, error) {
+	var (
+		isPrefix bool  = true
+		err      error = nil
+		line, ln []byte
+	)
+	for isPrefix && err == nil {
+		line, isPrefix, err = r.ReadLine()
+		ln = append(ln, line...)
+	}
+	return string(ln), err
+}
