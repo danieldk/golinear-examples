3701823290091fb8d0587434058b0eaf241a504e
Daniël de Kok <me@danieldk.eu>
2012-09-16 12:30:51 +0200

    Flatten directory structure.

--- word_classification/train/main.go
+++ word_classification/train/main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/danieldk/golinear"
-	"github.com/danieldk/golinear/examples/word_classification"
+	"github.com/danieldk/golinear-examples/word_classification"
 	"log"
 	"os"
 )
