aabea5b29632f70314e7782fee47f55afe503c94
Daniël de Kok <me@danieldk.eu>
2012-09-30 16:35:17 +0200

    Use built-in append type.

--- word_classification/util.go
+++ word_classification/util.go
@@ -4,16 +4,6 @@ import (
 	"bufio"
 )
 
-func AppendFeatureLists(l1, l2 []StringFeature) []StringFeature {
-	l := len(l1) + len(l2)
-
-	newSlice := make([]StringFeature, l)
-	copy(newSlice, l1)
-	copy(newSlice[len(l1):], l2)
-
-	return newSlice
-}
-
 func FilterDictionary(dict Dictionary, max uint64) {
 	for word, m := range dict {
 		var count uint64 = 0
